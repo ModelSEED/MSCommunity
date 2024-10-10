65e3ce36a0743f5a8e0383c51591b264c01477c2
Andrew's WorkMacbook <freiburgermsu@aol.com>
2024-10-10 10:23:30 -0500

    miscellaneous pyCache content

---
+++ build/lib/mscommunity/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+# import pyximport; pyximport.install(language_level=3)  # improve computational speed
+
+from mscommunity.mscommunity import *
+from mscommunity.commkineticpkg import CommKineticPkg
+from mscommunity.mssteadycom import MSSteadyCom
+from mscommunity.commhelper import *
+from mscommunity.mskineticsfba import MSKineticsFBA
